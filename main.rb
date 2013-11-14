bda1c12da46ad9970aa172fbbd1aead986fee381
Jose Casanova <josescasanova@gmail.com>
2013-11-14 12:57:03 -0500

    Added text cards and total to /game

--- main.rb
+++ main.rb
@@ -3,6 +3,63 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+end
+
 get '/' do
-  erb :new
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+
+  session[:player_name] = params[:player_name] 
+  redirect '/game'
+end
+
+get '/game' do
+  session[:turn] = session[:player_name]
+
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
 end
