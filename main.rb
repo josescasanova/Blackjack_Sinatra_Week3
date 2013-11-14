d86870b1ce286d2a3b416361a3dc90196f467588
Jose Casanova <josescasanova@gmail.com>
2013-11-14 15:25:10 -0500

    Completed basic project with play_again feature

--- main.rb
+++ main.rb
@@ -23,6 +23,47 @@ helpers do
 
     total
   end
+
+  def card_image(card)
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
+
+  def winner!(msg)
+    @show_hit_or_stay_buttons = false
+    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+  end
+
+  def loser!(msg)
+    @show_hit_or_stay_buttons = false
+    @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
+  end
+
+  def tie!(msg)
+    @show_hit_or_stay_buttons = false
+    @success = "<strong>It is a tie!</strong> #{msg}"
+  end
+end
+
+before do
+  @show_hit_or_stay_buttons = true
 end
 
 get '/' do
@@ -32,6 +73,8 @@ get '/' do
     redirect '/new_player'
   end
 end
+
+
 
 get '/new_player' do
   erb :new_player
@@ -62,4 +105,75 @@ get '/game' do
   session[:player_cards] << session[:deck].pop
 
   erb :game
+end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    winner!("Winner!")
+    @play_again = true
+  elsif player_total > 21
+    loser!("Loser!")
+    @play_again = true
+  end
+
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay."
+  @show_hit_or_stay_buttons = false
+  redirect '/game/dealer'
+end
+
+get '/game/dealer' do
+  session[:turn] = "dealer"
+
+  @show_hit_or_stay_buttons = false
+
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if dealer_total == 21
+    loser!("Sorry, dealer wins.")
+    @play_again = true
+  elsif dealer_total > 21
+    winner!("Congrats, dealer busted.")
+    @play_again = true
+  elsif dealer_total >= 17
+    redirect '/game/compare'
+  else
+    redirect '/game/dealer/hit'
+  end
+
+    erb :game
+end
+
+get '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/compare' do
+  @show_hit_or_stay_buttons = false
+
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    loser!("#{session[:player_name]} stayed at #{player_total} and the Dealer stayed at #{dealer_total}")
+    @play_again = true
+  elsif player_total > dealer_total
+    winner!("#{session[:player_name]} stayed at #{player_total} and the Dealer stated at #{dealer_total}")
+    @play_again = true
+  else
+    tie!("Both #{session[:player_name]} and Dealer stayed at #{player_total}")
+    @play_again = true
+  end
+
+  erb :game
+end
+
+get '/game_over' do
+  "Game over!"
 end
